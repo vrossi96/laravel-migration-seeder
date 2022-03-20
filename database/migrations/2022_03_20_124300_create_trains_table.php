cbb064e6e4ea738b64531eb94d3e8b78d286deb8
vrossi96 <vrossi396@yahoo.it>
2022-03-20 14:18:02 +0100

    add create trains table migration

--- database/migrations/2022_03_20_124300_create_trains_table.php
+++ database/migrations/2022_03_20_124300_create_trains_table.php
@@ -15,6 +15,15 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 75);
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time', 0);
+            $table->dateTime('arrival_time', 0);
+            $table->unsignedSmallInteger('train_code');
+            $table->unsignedTinyInteger('carriages');
+            $table->boolean('is_in_time')->nullable();
+            $table->boolean('is_cancelled')->nullable();
             $table->timestamps();
         });
     }
