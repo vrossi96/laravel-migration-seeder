85193737d48ebc75828f225d8e665e7994dc9d4d
vrossi96 <vrossi396@yahoo.it>
2022-03-22 15:21:41 +0100

    add fillable in Train model

--- app/Train.php
+++ app/Train.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $fillable = [
+        'company', 'departure_station', 'arrival_station', 'departure_time', 'arrival_time', 'train_code', 'carriages', 'is_in_time', 'is_cancelled'
+    ];
 }
